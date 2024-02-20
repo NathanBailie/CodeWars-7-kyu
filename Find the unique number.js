dc406e8ac8985adfcb8f57ce9cbb6bbd288e7e81
Nathan <antnefedov33@gmail.com>
2024-02-20 13:08:30 +0300

    Find the unique number

---
+++ Find the unique number.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/585d7d5adb20cf33cb000235
+
+function findUniq(arr) {
+    let uniqNums = [...new Set(arr)];
+    let count = 0;
+    for (let i = 0; i < 4; i++) arr[i] === uniqNums[0] && count++;
+    return count > 1 ? uniqNums[1] : uniqNums[0]
+}
