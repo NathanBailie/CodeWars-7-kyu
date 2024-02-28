056c4553eff67cda2833b18dac1231f9807ebed7
Nathan <antnefedov33@gmail.com>
2024-02-28 15:59:23 +0300

    Persistent Bugger

---
+++ Persistent Bugger.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+
+function persistence(num, count = 0) {
+    if (num < 10) return count;
+    const res = String(num).split('').reduce((acc, n) => acc * n, 1);
+    return persistence(res, count + 1);
+}
