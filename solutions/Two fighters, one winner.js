482552dbc3a96dffd4d6c0a83cc304c735930bf4
Nathan <antnefedov33@gmail.com>
2024-03-18 14:04:30 +0300

    Two fighters, one winner

---
+++ solutions/Two fighters, one winner.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/577bd8d4ae2807c64b00045b
+
+function declareWinner(f1, f2, firstAttacker) {
+    const hits1 = Math.ceil(f1.health / f2.damagePerAttack);
+    const hits2 = Math.ceil(f2.health / f1.damagePerAttack);
+    return hits1 === hits2 ? firstAttacker : (hits1 > hits2 ? f1.name : f2.name);
+}
