03c8f8dd7ee721ddd6271a6d758606136acb9fb6
Nathan <antnefedov33@gmail.com>
2024-03-22 13:11:52 +0300

    Highest Rank Number in an Array

---
+++ solutions/Highest Rank Number in an Array.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5420fc9bb5b2c7fd57000004
+
+function highestRank(arr) {
+    const counter = arr.reduce((obj, n) => { obj[n] ? obj[n] += 1 : obj[n] = 1; return obj }, {});
+    let maxN = 0, maxFr = 0;
+    for (let key in counter) {
+        counter[key] > maxFr && (maxN = key, maxFr = counter[key]);
+        counter[key] === maxFr && (maxN = Math.max(maxN, key));
+    }
+    return maxN;
+}
