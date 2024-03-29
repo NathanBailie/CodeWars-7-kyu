a56d772b68ea0f7de483ee4ab4b5b42190e6e1fa
Nathan <antnefedov33@gmail.com>
2024-03-29 15:12:40 +0300

    Base64 Encoding

---
+++ solutions/Base64 Encoding.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5270f22f862516c686000161
+
+String.prototype.toBase64 = function () {
+    return Buffer.from(this).toString('base64');
+};
+
+String.prototype.fromBase64 = function () {
+    return Buffer.from(this, 'base64').toString();
+};
