a2daceed498d35c8ea09d032af0dac63b5da4ade
Nathan <antnefedov33@gmail.com>
2024-04-01 16:34:26 +0300

    Largest product in a series

---
+++ solutions/Largest product in a series.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/529872bdd0f550a06b00026e
+
+function greatestProduct(str) {
+    let max = 0;
+    for (let i = 0; i <= str.length - 5; i++) {
+        let num = str.slice(i, i + 5).split('').reduce((acc, n) => acc * (+n), 1);
+        max = Math.max(max, num);
+    }
+    return max;
+}
