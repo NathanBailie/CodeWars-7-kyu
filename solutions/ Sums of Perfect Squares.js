8698c724fda7ada13b7136b69fcf926ff32e56ac
Nathan <antnefedov33@gmail.com>
2024-04-23 15:01:42 +0300

    Sums of Perfect Squares

---
+++ solutions/ Sums of Perfect Squares.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5a3af5b1ee1aaeabfe000084
+
+function sumOfSquares(n) {
+    while (n % 4 === 0) n /= 4;
+    if (n % 8 === 7) return 4;
+    for (let a = 0; a * a <= n; a++) {
+        let b = Math.floor(Math.sqrt(n - a * a));
+        if (a * a + b * b === n) return !!a + !!b;
+    }
+    return 3;
+}
