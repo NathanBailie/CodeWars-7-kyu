d53aae5945e93d432bd482bac780b9ab84e8bd29
Nathan <antnefedov33@gmail.com>
2024-05-17 16:25:59 +0300

    I Spy

---
+++ solutions/I Spy.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/555185132c0d4cca3d000197
+
+function spyOn(func) {
+    let calls = [];
+
+    function spy(...args) {
+        calls.push({ args, result: func(...args) });
+        return calls[calls.length - 1].result;
+    }
+
+    spy.callCount = () => calls.length;
+    spy.wasCalledWith = n => calls.some(call => call.args.includes(n));
+    spy.returned = n => calls.some(call => call.result === n);
+
+    return spy;
+}
