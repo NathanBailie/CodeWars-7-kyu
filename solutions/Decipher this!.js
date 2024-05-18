0d9e41bcf7c63489788d0fd3a30b6dba350592f4
Nathan <antnefedov33@gmail.com>
2024-05-18 16:16:06 +0300

    Decipher this!

---
+++ solutions/Decipher this!.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/581e014b55f2c52bb00000f8
+
+function decipherThis(str) {
+    return str.replace(/\d+/g, n => String.fromCharCode(n))
+        .replace(/\b(\w)(\w)(\w*)(\w)\b/g, (_, a, b, c, d) => a + d + c + b);
+}
