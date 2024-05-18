bc5b55ae1780d2aab9a21831a204545aaf7fbb47
Nathan <antnefedov33@gmail.com>
2024-05-18 16:31:33 +0300

    Vector class

---
+++ solutions/Vector class.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/526dad7f8c0eb5c4640000a4
+
+class Vector {
+    constructor(elems) {
+        this.elems = elems;
+    }
+
+    add(v) {
+        this.checkLength(v);
+        return new Vector(this.elems.map((n, i) => n + v.elems[i]));
+    }
+
+    subtract(v) {
+        this.checkLength(v);
+        return new Vector(this.elems.map((n, i) => n - v.elems[i]));
+    }
+
+    dot(v) {
+        this.checkLength(v);
+        return this.elems.reduce((acc, n, i) => acc + n * v.elems[i], 0);
+    }
+
+    norm() {
+        return Math.sqrt(this.elems.reduce((acc, n) => acc + n ** 2, 0));
+    }
+
+    toString() {
+        return `(${this.elems.join(',')})`;
+    }
+
+    equals(v) {
+        return this.elems.every((n, i) => n === v.elems[i]);
+    }
+
+    checkLength(v) {
+        if (this.elems.length !== v.elems.length) {
+            throw new Error('Different vector lengths!');
+        }
+    }
+}
