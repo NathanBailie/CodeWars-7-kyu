0e51e0a4d6efe44c0fc89e43a45fe3d2c5ed10d8
Nathan <antnefedov33@gmail.com>
2024-05-20 12:03:51 +0300

    extract portion of file name

---
+++ solutions/extract portion of file name.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/597770e98b4b340e5b000071
+
+class FileNameExtractor {
+    static extractFileName(dirtyFileName) {
+        return dirtyFileName.replace(/^\d+_|(\.\w+)(\..+)/g, '$1');
+    }
+}
