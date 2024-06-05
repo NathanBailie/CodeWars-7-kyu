d2a8b3fdd15055bcdb094520e1bfcdec1ef7fa4f
Nathan <antnefedov33@gmail.com>
2024-06-05 18:02:40 +0300

    Row Weights

---
+++ solutions/Row Weights.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5abd66a5ccfd1130b30000a9
+
+function rowWeights(arr) {
+    let t1 = 0, t2 = 0;
+    arr.forEach((n, i) => i % 2 === 0 ? t1 += n : t2 += n);
+    return [t1, t2];
+}
