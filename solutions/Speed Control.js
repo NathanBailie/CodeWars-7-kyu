fbd3c3f50f211e51e04b32be1a4f877d3fd5b5db
Nathan <antnefedov33@gmail.com>
2024-06-08 16:33:13 +0300

    Speed Control

---
+++ solutions/Speed Control.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56484848ba95170a8000004d
+
+function gps(s, x) {
+    let max = 0;
+    x.forEach((n, i, arr) => i > 0 && (max = Math.max(max, Math.floor((3600 * (n - arr[i - 1])) / s))));
+    return max;
+}
