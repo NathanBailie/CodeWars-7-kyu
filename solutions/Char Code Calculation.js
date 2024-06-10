d5d5ff6db62c8fd44da33198dce3686d5ff55271
Nathan <antnefedov33@gmail.com>
2024-06-10 18:16:28 +0300

    Char Code Calculation

---
+++ solutions/Char Code Calculation.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/57f75cc397d62fc93d000059
+
+function calc(str) {
+    function getSumOfDigits(strNum) {
+        let sum = 0;
+        for (let n of strNum) sum += +n;
+        return sum;
+    }
+
+    let num1 = str.replace(/\w/g, c => c.charCodeAt());
+    let num2 = num1.replace(/7/g, '1');
+
+    return getSumOfDigits(num1) - getSumOfDigits(num2);
+}
