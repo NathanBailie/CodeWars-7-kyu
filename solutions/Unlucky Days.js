bb8abef3dba9f7abefd7c3309938543b5b55d729
Nathan <antnefedov33@gmail.com>
2024-06-13 16:17:46 +0300

    Unlucky Days

---
+++ solutions/Unlucky Days.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/56eb0be52caf798c630013c0
+
+function unluckyDays(year) {
+    let count = 0, month = 0;
+
+    while (month < 12) {
+        new Date(year, month, 13).getDay() === 5 && count++;
+        month++;
+    }
+
+    return count;
+}
