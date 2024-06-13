60c3a8743ebb6e5d759d7d3f2c888222175a91f9
Nathan <antnefedov33@gmail.com>
2024-06-13 15:46:34 +0300

    Jumping Number (Special Numbers Series #4)

---
+++ solutions/Jumping Number (Special Numbers Series #4).js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5a54e796b3bfa8932c0000ed
+
+function jumpingNumber(n) {
+    if (n < 10) return 'Jumping!!';
+    let strNum = String(n);
+
+    for (let i = 1; i < strNum.length; i++) {
+        if (Math.abs(strNum[i] - strNum[i - 1]) !== 1) return "Not!!";
+    }
+
+    return "Jumping!!";
+}
