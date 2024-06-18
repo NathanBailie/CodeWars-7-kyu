a4ce8b2646b0ef3a58d69ef740f86d5bd45a160c
Nathan <antnefedov33@gmail.com>
2024-06-18 16:40:45 +0300

    Play with two Strings

---
+++ solutions/Play with two Strings.js
@@ -0,0 +1,26 @@
+// https://www.codewars.com/kata/56c30ad8585d9ab99b000c54
+
+function workOnStrings(s1, s2) {
+    let str1 = changeStr(s1, s2);
+    let str2 = changeStr(s2, s1);
+    return `${str1}${str2}`;
+}
+
+function changeStr(s1, s2) {
+    let res = '';
+    let hash = {};
+    for (let char of s2.toLowerCase()) hash[char] = (hash[char] || 0) + 1;
+    for (let char of s1) {
+        let amount = hash[char.toLowerCase()] || 0;
+        res += changeCase(char, amount);
+    }
+    return res;
+}
+
+function changeCase(char, n) {
+    let res = char;
+    for (let i = 0; i < n; i++) {
+        res = res.toUpperCase() === res ? res.toLowerCase() : res.toUpperCase();
+    }
+    return res;
+}
